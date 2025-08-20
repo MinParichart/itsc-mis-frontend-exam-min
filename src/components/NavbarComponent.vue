caa847fecb85eec08f7d62b1c7ac654b70b6499c
MinChatr <min_etc@hotmail.com>
2025-08-20 08:53:04 +0700

    structure vue

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="bg-white border-b border-gray flex flex-nowrap container">
+    <div class="justify-end flex flex-nowrap">
+      <h2>บทความ</h2>
+      <button class="bg-blue-500 text-white rounded-l">log out</button>
+    </div>
+  </nav>
+</template>
+
+
+
+<script setup lang="ts">
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavbarComponent from '@/components/NavbarComponent.vue';
+
+</script>
+
+<template>
+
+  <NavbarComponent></NavbarComponent>
+
+</template>
+
+<style scoped>
+
+</style>
