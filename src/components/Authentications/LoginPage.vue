c386d08a23ad13a7861cce34b62772cff05e4afd
MinParichatr <parichatr_ya@cmu.ac.th>
2025-09-17 08:43:56 +0700

    LoginPage.vue

--- src/components/Authentications/LoginPage.vue
+++ src/components/Authentications/LoginPage.vue
@@ -12,7 +12,7 @@ const password = ref("");
 const err = ref("");
 const loading = ref(false);
 
-const r = useRouter();
+const router = useRouter();
 const route = useRoute();
 
 // CHANGE 2: ประกาศชนิดข้อมูลที่คาดหวังจาก API
@@ -40,7 +40,7 @@ async function login() {
       String(Date.now() + data.expires_in * 1000)
     ); // 5 นาที
 
-    r.push((route.query.redirect as string) || "/blogs");
+    router.push((route.query.redirect as string) || "/blogs");
   } catch (e: unknown) {
     const ax = e as AxiosError<any>;
     const status = ax.response?.status;
